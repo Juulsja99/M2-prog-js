f8ebc3cd884e0655e160dbd93924d23645bbba35
Julie Jaasma <31329@ma-web.nl>
2022-11-24 10:34:51 +0100

    opdracht1

---
+++ les01/app.js
@@ -0,0 +1,20 @@
+class App {
+
+    runApplication() {
+        console.log("Hello world!");
+        let appnaam = "testJavascript";
+        let versienummer = 1.0;
+        let versiedatum = 24112022;
+        let autheur = "JulieJaasma";
+        let copyright = "idk";
+        let distributer = "geenidee";
+        let darkmode = true;
+
+        console.log(appnaam, versienummer, versiedatum, autheur, copyright, distributer, darkmode,);
+
+    }
+}
+let app = new App();
+app.runApplication();
+
+
